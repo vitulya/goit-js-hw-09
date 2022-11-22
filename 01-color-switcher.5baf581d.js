7aba4da8e86910419f2d97dbef3ea3f726069073
vitulya <108943265+vitulya@users.noreply.github.com>
2022-11-22 18:40:06 +0000

    Deploying to gh-pages from @ vitulya/goit-js-hw-09@162940d977b5bac60f51cb50bc5ec28a4ea15965 🚀

---
+++ 01-color-switcher.5baf581d.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(function(n){intervalId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(intervalId),t.btnStart.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5baf581d.js.map
