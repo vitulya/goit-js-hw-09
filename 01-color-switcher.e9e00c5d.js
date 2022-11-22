43f52f7f9e49a40739be584a5809042aeb2a0e3d
vitulya <108943265+vitulya@users.noreply.github.com>
2022-11-22 18:45:09 +0000

    Deploying to gh-pages from @ vitulya/goit-js-hw-09@fb613348ea1501c448d975a2fea337b270a02899 🚀

--- 01-color-switcher.e9e00c5d.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(e=>{intervalId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(()=>{clearInterval(intervalId),t.btnStart.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.e9e00c5d.js.map
